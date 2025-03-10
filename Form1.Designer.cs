2a4b6a441b81b5aefa3f6a653a65d466b14cbdaf
pinxun93 <pinxunlai0327@gmail.com>
2025-03-10 15:53:25 +0800

    第二次更新
    1. 公分 -> 公尺、公里、英吋、英呎、碼
    2. 公尺 -> 公分、公里、英吋、英呎、碼
    3. 公里 -> 公分、公尺、英吋、英呎、碼

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -148,6 +148,7 @@ namespace LengthCalculator
             this.txtM.Name = "txtM";
             this.txtM.Size = new System.Drawing.Size(87, 25);
             this.txtM.TabIndex = 10;
+            this.txtM.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtM_KeyUp);
             // 
             // txtKM
             // 
@@ -155,6 +156,7 @@ namespace LengthCalculator
             this.txtKM.Name = "txtKM";
             this.txtKM.Size = new System.Drawing.Size(87, 25);
             this.txtKM.TabIndex = 11;
+            this.txtKM.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtKM_KeyUp);
             // 
             // txtIn
             // 
--- Form1.cs
+++ Form1.cs
@@ -20,12 +20,40 @@ namespace LengthCalculator
         private void txtCM_KeyUp(object sender, KeyEventArgs e)
         {
             double douCM; //宣告一個double變數，變數名稱叫douCM
-
             douCM = Convert.ToDouble(txtCM.Text); //從txtCM輸入文字框取得輸入的文字，並且轉換成double的資料型態
 
-            txtM.Text = string.Format("{0:0.##########}", douCM / 100);
-            //將douCM的數值除以100，也就是從公分轉換成公尺
             //透過string.Format格式化成小數點後共10位的數字，轉型成文字型態，在txtM顯示結果
+            txtM.Text = string.Format("{0:0.##########}", douCM / 100);
+            txtKM.Text = string.Format("{0:0.##########}", douCM / 100000);
+            txtIn.Text = string.Format("{0:0.##########}", douCM / 2.54);
+            txtFt.Text = string.Format("{0:0.##########}", douCM / 30.48);
+            txtYard.Text = string.Format("{0:0.##########}", douCM / 91.44);
+        }
+
+        private void txtM_KeyUp(object sender, KeyEventArgs e)
+        {
+            double douM; //宣告一個double變數，變數名稱叫douM
+            douM = Convert.ToDouble(txtM.Text); //從txtM輸入文字框取得輸入的文字，並且轉換成double的資料型態
+
+            //透過string.Format格式化成小數點後共10位的數字，轉型成文字型態，在txtM顯示結果
+            txtCM.Text = string.Format("{0:0.##########}", douM * 100);
+            txtKM.Text = string.Format("{0:0.##########}", douM *0.001);
+            txtIn.Text = string.Format("{0:0.##########}", douM * 39.3701);
+            txtFt.Text = string.Format("{0:0.##########}", douM * 3.28084);
+            txtYard.Text = string.Format("{0:0.##########}", douM * 1.09361);
+        }
+
+        private void txtKM_KeyUp(object sender, KeyEventArgs e)
+        {
+            double douKM; //宣告一個double變數，變數名稱叫douKM
+            douKM = Convert.ToDouble(txtKM.Text); //從txtKM輸入文字框取得輸入的文字，並且轉換成double的資料型態
+
+            //透過string.Format格式化成小數點後共10位的數字，轉型成文字型態，在txtKM顯示結果
+            txtCM.Text = string.Format("{0:0.##########}", douKM * 100000);
+            txtM.Text = string.Format("{0:0.##########}", douKM * 1000);
+            txtIn.Text = string.Format("{0:0.##########}", douKM * 39370.1);
+            txtFt.Text = string.Format("{0:0.##########}", douKM * 3280.84);
+            txtYard.Text = string.Format("{0:0.##########}", douKM * 1093.61);
         }
     }
 }
